74394ceaf3c4bda96fb631f7a89e7f0eceed513c
Elias Cooper <coopere905@gmail.com>
2025-10-24 15:56:05 -0400

    Change default values for UNANET and DATAVERSE config
    Updated environment variable defaults to empty strings.

--- config.py
+++ config.py
@@ -16,17 +16,17 @@ env_path = application_path / '.env'
 load_dotenv(dotenv_path=env_path)
 
 # === UNANET CONFIG ===
-UNANET_URL = os.getenv('UNANET_URL', 'https://goaztech.unanet.biz')
+UNANET_URL = os.getenv('UNANET_URL', 'h')
 UNANET_USERNAME = os.getenv('UNANET_USERNAME', '')
 UNANET_PASSWORD = os.getenv('UNANET_PASSWORD', '')
-UNANET_REPORT_ID = os.getenv('UNANET_REPORT_ID', 'R_91')
+UNANET_REPORT_ID = os.getenv('UNANET_REPORT_ID', '')
 
 # === DATAVERSE/POWERAPPS CONFIG ===
-DATAVERSE_URL = os.getenv('DATAVERSE_URL', 'https://org4bd86d49.crm.dynamics.com')
+DATAVERSE_URL = os.getenv('DATAVERSE_URL', '')
 DATAVERSE_USERNAME = os.getenv('DATAVERSE_USERNAME', '')
 DATAVERSE_PASSWORD = os.getenv('DATAVERSE_PASSWORD', '')
-TABLE_PREFIX = os.getenv('TABLE_PREFIX', 'cr834')
-TABLE_NAME = os.getenv('TABLE_NAME', f"{TABLE_PREFIX}_eacdataraws")
+TABLE_PREFIX = os.getenv('TABLE_PREFIX', '')
+TABLE_NAME = os.getenv('TABLE_NAME', '')
 
 # === FILE PATHS ===
 PROJECT_DIR = application_path
